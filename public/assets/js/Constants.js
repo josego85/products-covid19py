8f080926f4cee534cbf36c06f75a25f30adfe4f0
josego <josego85@gmail.com>
2020-04-08 00:36:24 -0400

    Se borra el archivo porque ya no se usa mas.

--- public/assets/js/Constants.js
+++
@@ -1,6 +0,0 @@
-var HOSTNAME = "http://localhost/products-covid19py/public/";
-var HOSTNAME_API = "http://api-products-covid19py/";
-
-// Asuncion - Paraguay.
-var DEFAULT_LNG = -57.623807;
-var DEFAULT_LAT = -23.299114;
