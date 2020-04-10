47b3ef9957f757890064d5c9c1d6e84a799b37d1
josego <josego85@gmail.com>
2020-04-10 19:33:41 -0400

    Se borra la constante que ya no e usa mas.

--- public/assets/js/Constants.js
+++
@@ -1,7 +0,0 @@
-var HOSTNAME = 'http://localhost/products-covid19py/public/';
-var HOSTNAME_API = HOSTNAME + 'api/';
-var GOOGLE_ANALYTICS_CODE = '';
-
-// Asuncion - Paraguay.
-var DEFAULT_LNG = -57.623807;
-var DEFAULT_LAT = -23.299114;
