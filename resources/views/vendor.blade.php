5389d579a29b3af39180b365c814f46cc8703e8c
josego <josego85@gmail.com>
2020-04-11 01:33:36 -0400

    Mejoras visuales en la carga del vendedor.

--- resources/views/vendor.blade.php
+++ resources/views/vendor.blade.php
@@ -14,7 +14,7 @@
     <header class="jumbotron my-4">
         <h2 class="display-5">Dar de alta vendedor</h2>
         <p class="lead">
-            Completa los datos del vendedor con los productos para aparecer en el sitio.
+            Completa los datos del vendedor con los productos para aparecer en ProductosPY.
             </br>
             En caso de no encontrarse su producto, contactarse a <strong>productospy.org@gmail.com</strong>
         </p>
@@ -35,6 +35,7 @@
                     {
                         let response = JSON.parse(data);
                         let status = response.status;
+                        let result = response.result;
 
                         if (status)
                         {
@@ -67,47 +68,55 @@
         <form id="form" role="form" method='post'>
             <div class="form-group">
                 <label>
-                    Email
+                    <span class="label label-default" style="font-size:22px;">Email</span>
                     <span>(opcional)</span>
                 </label>
                 <input class="form-control" type="text" placeholder="Ingrese aqu&iacute; el email del vendedor" value="" name="user_email" size="25" />
             </div>
             <div class="form-group">
                 <label>
-                    Nombre y apellido
+                    <span class="label label-default" style="font-size:22px;">Nombre y apellido</span>
                     <span>(opcional)</span>
                 </label>
                 <input class="form-control" type="text" placeholder="Ingrese aqu&iacute; el nombre y apellido del vendedor" name="user_full_name" value="" size="25" />
             </div>
             <div class="form-group">
                 <label>
-                    N&uacute;mero de contacto
+                    <span class="label label-default" style="font-size:22px;">N&uacute;mero de contacto</span>
                     <span>(*)</span>
                 </label>
                 <input class="form-control" type="text" placeholder="Ingrese aqu&iacute; el n&uacute;mero de contacto del vendedor" name="user_phone" value="" size="25" />
             </div>
             <div class="form-group">
                 <label>
-                    Productos
+                    <span class="label label-default" style="font-size:22px;">Productos</span>
                     <span>(*)</span>
                 </label>
                 @include('products')
             </div>
             <div>
                 <label>
-                    Ubicaci&oacute;n del vendedor
+                    <span class="label label-default" style="font-size:22px;">Ubicaci&oacute;n del vendedor</span>
                     <span>(opcional)</span>
                     </br>
-                    Si tenes una ubicaci&oacute;n geogr&aacute;fica haz click en el bot&oacute;n de abajo para que 
-                    aparezca en el mapa un marcador y puedas moverlo para indicar tu ubicaci&oacute;n.
-                    </br>
-                    Si no moves el marcador no va a guardar tu ubicaci&oacute;n!!!
-                    </br>
-                    <button onclick="marker_point_map(event, DEFAULT_ZOOM_MAP)">Marcar ubicaci&oacute;n</button>
-                    
+                    <div class="alert alert-info" role="alert">
+                        <h4 class="alert-heading">Aviso!</h4>
+                        <p>
+                            Si tenes una ubicaci&oacute;n geogr&aacute;fica haz click en el bot&oacute;n de abajo para que 
+                            aparezca en el mapa un marcador y puedas moverlo para indicar tu ubicaci&oacute;n.
+                        </p>
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                        <h4 class="alert-heading">Observaci&oacute;n!</h4>
+                        <p>
+                            Si no moves el marcador no va a guardar tu ubicaci&oacute;n!!!
+                        </p>
+                    </div>
+                    <button type="button" class="btn btn-success btn-lg btn-block" onclick="marker_point_map(event, DEFAULT_ZOOM_MAP)">Marcar ubicaci&oacute;n</button>
                 </label>
+                </br>
                 <div id="map">
-                    <div id='map-container' style="height: 300px; border: 1px solid #AAA;"></div>
+                    <div id='map-container' style="width: 100%; height: 300px; border: 1px solid #AAA;"></div>
                 </div>
             </div>
             <div class="form-group">
@@ -116,7 +125,7 @@
             <div class="form-group">
                 <input class="form-control" type="hidden" name="user_lng" id="user_lng" value="" placeholder="click en el mapa"/>
             </div>
-            <input class="btn btn-primary" type="submit" name="submit" value="Enviar" />
+            <input class="btn btn-primary btn-lg btn-block" type="submit" name="submit" value="Enviar" />
        </form>
        <div id="msg">
        </div>
