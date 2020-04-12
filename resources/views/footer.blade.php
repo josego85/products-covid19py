8139502e3c2fe856f87fc85d37a42d18f06034c7
josego <josego85@gmail.com>
2020-04-12 13:27:55 -0400

    Se arregla el link del repositorio.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <p class="m-0 text-center text-white"><a href="{{ url('disclamer') }}"><strong>T&eacute;rminos de uso</strong></a></p>
         <p class="m-0 text-center text-white">Copyright &copy; Proyectos Beta 2020</p>
-        <p class="m-0 text-center text-white"><a href="https://github.com/josego85/products_covid19py" target="_blank"><strong>Git Hub</strong></a></p>
+        <p class="m-0 text-center text-white"><a href="https://github.com/josego85/products-covid19py" target="_blank"><strong>Git Hub</strong></a></p>
     </div>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
