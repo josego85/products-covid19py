83c3a11076011a30b6f40c5023a0157d88ff3b39
proyectosbeta <josego85@gmail.com>
2020-06-09 14:37:41 -0400

    Refactorización de código.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Cities as citiesModel;
 
-class Main extends Controller
-{   
+class HomeController extends Controller
+{
+    /**
+     * Show the application home.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         // Fetch cities.
         $citiesModel = new citiesModel();
         $cities = $citiesModel->getCities();
-
+ 
         return view('main')->with("cities", $cities);
-      }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-Route::get('/', 'Main@index');
+Route::get('/', 'HomeController@index');
 
 Route::get('/disclaimer', function () {
     return view('pages.disclaimer');
